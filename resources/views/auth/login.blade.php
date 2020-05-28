46f843b3154e4185681b7c89d3e111e3a6969146
muhammadardie <ardiemuhammad3@gmail.com>
2020-05-28 14:01:40 +0700

    update login page title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,24 +8,26 @@
 
 		<!--end::Base Path -->
 		<meta charset="utf-8" />
-		<title>Duaz Backup</title>
+		<title>Database Backup</title>
 		<meta name="description" content="Latest updates and statistic charts">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		{{-- Favicon --}}
         <link rel="icon" href="{{ asset('db.ico') }}">
-        {{-- css --}}
-        <!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <link href="assets/css/login.css" rel="stylesheet" type="text/css" />
-        {{-- script --}}
-        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-		<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
-        <script src="vendor/jsvalidation/js/jsvalidation.min.js" type="text/javascript"></script>
+
+        <!--   Core JS Files   -->
+	    <script src="assets/js/core/jquery.3.2.1.min.js"></script>
+	    <script src="assets/js/core/popper.min.js"></script>
+	    <script src="assets/js/core/bootstrap.min.js"></script>
+
+    	<!-- CSS Files -->
+    	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    	<link href="assets/css/login.css" rel="stylesheet" type="text/css" />
+    	
 	</head>    
 	<body>
 		<div class="login">
-			<h1>Duaz Backup</h1>
+			<h1>Database Backup</h1>
 		    <form id="login-form" role="form" class="kt-form" action="{{ route('login') }}" method="post">
 		        {{ csrf_field() }}
 		    	<input type="email" placeholder="Email" name="email" required/>
