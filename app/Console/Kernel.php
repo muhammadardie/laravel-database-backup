8519ed3f21282e7a3b73379df92d882fffe4ca26
muhammadardie <ardiemuhammad3@gmail.com>
2021-12-24 16:59:29 +0700

    set task run daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('backupDb:daily')->everyMinute();
+        $schedule->command('backupDb:daily')->daily();
     }
 
     /**
